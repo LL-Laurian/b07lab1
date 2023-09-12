f90a060b07f3f64972f9959d8229b0ee37488280
laurian_li <laurian.li@mail.utoronto.ca>
2023-09-12 16:18:29 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,47 @@
+
+public class Polynomial {
+    double[] coefficient;
+
+    public Polynomial(){
+        coefficient = new double[]{0};
+    }
+
+    public Polynomial(double[] coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public Polynomial add (Polynomial src_polynomial) {
+        int max_length = Math.max(coefficient.length, src_polynomial.coefficient.length);
+        double[] result = new double[max_length];
+
+        for (int i = 0; i < max_length; i++) {
+            if (i >= coefficient.length) {
+                result[i] = 0 + src_polynomial.coefficient[i];
+            } else if (i >= src_polynomial.coefficient.length) {
+                result[i] = 0 + coefficient[i];
+            } else {
+                result[i] = coefficient[i] + src_polynomial.coefficient[i];
+            }
+        }
+        Polynomial p_result = new Polynomial(result);
+        return p_result;
+
+    }
+
+    public double evaluate (double x){
+        double result = 0;
+        for (int i = 0; i < coefficient.length; i++){
+            result = coefficient[i] * Math.pow(x,i) + result;
+        }
+        return result;
+    }
+
+    public boolean hasRoot (double x){
+        if (evaluate(x) == 0){
+            return true;
+        }
+        else{return false;}
+    }
+}
+
+
